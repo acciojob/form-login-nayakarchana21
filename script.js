eda81a1449b1557b249a9e41948ad254ecaf4d14
Archana Nayak <archananayak212002@gmail.com>
2025-06-06 11:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
-    //Write your code here
-
+    
+    // Prevent form submission (if needed)
+    event.preventDefault();
+    
+    // Access the form using document.forms
+    const form = document.forms[0];
+    
+    // Get values from the input fields
+    const firstName = form.fname.value;
+    const lastName = form.lname.value;
+    
+    // Display full name using alert
+    alert(firstName + " " + lastName);
 }
+
+
+
